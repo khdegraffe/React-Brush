94d1dfe9a73b19b1b29db9aa020b73d8212ddf79
khdegraffe <khdegraffe@gmail.com>
2025-09-11 19:37:03 -0400

    add src files

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
 function App() {
-  return <AnimalShow message="Hi There" />;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="container">
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Number of animals: {count}</div>
+    </div>
+  );
 }
 
 export default App;
