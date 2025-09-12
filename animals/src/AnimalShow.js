8c22523a3ea7b82e1494bb31273c88f5ec392ef3
khdegraffe <khdegraffe@gmail.com>
2025-09-11 20:25:59 -0400

    add styling

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,5 +1,40 @@
-function AnimalShow({ message }) {
-  return <h1>{message}</h1>;
+import "./AnimalShow.css";
+import { useState } from "react";
+import bird from "./svg/bird.svg";
+import dog from "./svg/dog.svg";
+import cat from "./svg/cat.svg";
+import gator from "./svg/gator.svg";
+import cow from "./svg/cow.svg";
+import horse from "./svg/horse.svg";
+import heart from "./svg/heart.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  gator,
+  horse,
+  dog,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt="animal" src={svgMap[type]} />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
+    </div>
+  );
 }
 
 export default AnimalShow;
