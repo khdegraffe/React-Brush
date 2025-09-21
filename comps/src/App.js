3cedd41f9f770c81d2780cbdb1b56ba415ac7709
khdegraffe <khdegraffe@gmail.com>
2025-09-21 16:14:31 -0400

    inital commit

---
+++ comps/src/App.js
@@ -0,0 +1,40 @@
+import { GoBell, GoEyeClosed } from "react-icons/go";
+import { FaLinkedin } from "react-icons/fa";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button primary rounded>
+          <GoBell />
+          Hello?
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <GoEyeClosed />
+          Is anyone there?
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          <FaLinkedin />
+          ...anybody??
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          Tailwind Baby!!
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded outline>
+          Classes for Days
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ comps/src/components/Button.js
@@ -0,0 +1,51 @@
+import className from "classnames";
+import { twMerge } from "tailwind-merge";
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
+  const classes = twMerge(
+    className("flex items-center px-3 py-1.5 border cursor-pointer", {
+      "border-blue-600 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-600 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-600 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-greeb-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    })
+  );
+
+  return <button className={classes}>{children}</button>;
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of Primary, Secondary, Success, Warning, Danger can be true"
+      );
+    }
+  },
+};
+
+export default Button;
