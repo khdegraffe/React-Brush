512c94d58f2acd5c35208304e01ded4d76a3ea0c
khdegraffe <khdegraffe@gmail.com>
2025-09-21 10:09:02 -0400

    add context use of BookList

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -3,17 +3,10 @@ import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList() {
-  const { books, editBookById, deleteBookById, Id } = useContext(BooksContext);
+  const { books } = useContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow
-        onEdit={editBookById}
-        onDelete={deleteBookById}
-        key={book.id}
-        book={book}
-      />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
   return <div className="book-list">{renderedBooks}</div>;
