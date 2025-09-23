ceea8e49b0c370f7b49c1b41dc8f7729d3740b54
khdegraffe <khdegraffe@gmail.com>
2025-09-23 13:02:49 -0400

    add handleClick accordion func

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (nextIndex) => setExpandedIndex(nextIndex);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
     return (
       <div key={item.id}>
-        <div>{items.label}</div>
-        <div>{item.content}</div>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
