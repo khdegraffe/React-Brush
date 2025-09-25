3ae0396534e0e3a3ba4d74ac14eb3e955555bacd
khdegraffe <khdegraffe@gmail.com>
2025-09-25 12:39:29 -0400

    add initial styling

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
   const options = [
     { label: "Red", value: "red" },
     { label: "Green", value: "green" },
@@ -8,7 +15,7 @@ function App() {
   ];
   return (
     <div>
-      <Dropdown options={options} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
     </div>
   );
 }
