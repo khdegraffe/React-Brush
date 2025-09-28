90916a4952f1c10e0b7a91f12f5850c9af92731e
khdegraffe <khdegraffe@gmail.com>
2025-09-28 14:29:30 -0400

    add route accordionPage && dropdownPage

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/accordion">Go to accordion</Link>
+      <br />
+      <Link to="/dropdown">Go to dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
+    </div>
+  );
 }
 
 export default App;
