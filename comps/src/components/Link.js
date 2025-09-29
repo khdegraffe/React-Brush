b5681530c27d16842160b17b78aabfb75e317da9
khdegraffe <khdegraffe@gmail.com>
2025-09-29 12:47:23 -0400

    add use-navigation hook

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -1,16 +1,25 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+import useNavigation from "../hooks/use-navigation";
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+
+  const classes = classNames("text-blue-500");
 
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     navigate(to);
     console.log("User navigating to:", to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
