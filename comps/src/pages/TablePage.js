1656abc0e9f6090d53766643b1643cf37250608e
khdegraffe <khdegraffe@gmail.com>
2025-09-30 20:21:38 -0400

    add styling for initial table comp

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,16 +1,16 @@
 import Table from "../components/Table";
 
 function TablePage() {
-  const fruits = [
+  const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
-    { name: "Apple", color: "bg-red-300", score: 3 },
-    { name: "Banana", color: "bg-yellow-500", score: 1 },
-    { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-300", score: 1 },
+    { name: "Lime", color: "bg-green-400", score: 4 },
   ];
 
   return (
     <div>
-      <Table fruits={fruits} />
+      <Table data={data} />
     </div>
   );
 }
